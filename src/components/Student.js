7b8df1b37efb18fa9e7eef0026d02ca52a50419b
Bakary Sylla <syllabakary2002@gmail.com>
2023-02-01 10:00:45 -0800

    css added to Student page responsive page

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,28 +1,47 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { Container } from "@mui/system";
+import { Paper } from "@mui/material";
 
 export default function Student() {
+  const PaperStyle = {
+    padding: "50px 20px",
+    width: "600",
+    margin: "20px auto",
+  };
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" />
-      <TextField
-        id="outlined-basic"
-        label="Student Address"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-basic"
-        label="Student Message"
-        variant="outlined"
-      />
-    </Box>
+    <Container>
+      <Paper elevation={3} style={PaperStyle}>
+        <h1 style={{ color: "blue" }}> Add Student & Message :) </h1>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Student Name"
+            variant="outlined"
+            fullWidth
+          />
+          <TextField
+            id="outlined-basic"
+            label="Student Address"
+            variant="outlined"
+            fullWidth
+          />
+          <TextField
+            id="outlined-basic"
+            label="Student Message"
+            variant="outlined"
+            fullWidth
+          />
+        </Box>
+      </Paper>
+    </Container>
   );
 }
